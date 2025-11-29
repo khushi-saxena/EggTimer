da49450e663bc091726ac528447bc77761dd3162
khushi-saxena <khushisaxena123123@gmail.com>
2025-11-28 16:29:08 -0800

    Add timer functionality to ViewController

--- ViewController.swift
+++ ViewController.swift
@@ -4,13 +4,27 @@ class ViewController: UIViewController {
     
   
     
-    let eggTimes = ["Soft": 5, "Medium":7, "Hard":12]
+    let eggTimes = ["Soft": 3, "Medium":4, "Hard":7]
+    
+    var secondsRemaining = 60
+    
+    var timer = Timer()
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
        
         let hardness = sender.currentTitle!
-        print (eggTimes[hardness]!)
+        secondsRemaining = eggTimes[hardness]!
+        
+        Timer.scheduledTimer(timeInterval: 1.0, target:self, selector: #selector(updateTimer), userInfo:nil, repeats:true)
       
+    }
+    @objc func updateTimer(){
+        if secondsRemaining > 0 {
+            print("\(secondsRemaining)seconds.")
+            secondsRemaining -= 1
+        }
     }
     
 }
