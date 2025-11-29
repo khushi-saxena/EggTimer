6e4fee8f0472018db378b2361c27f1c721343f3f
khushi-saxena <khushisaxena123123@gmail.com>
2025-11-28 16:13:09 -0800

    Update ViewController.swift

--- ViewController.swift
+++ ViewController.swift
@@ -1,22 +1,17 @@
-//
-//  ViewController.swift
-//  EggTimer
-//
-//  Created by Angela Yu on 08/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
     
-    let softTime = 5
-    let mediumTime = 7
-    let hardTime = 12
-
+  
+    
+    let eggTimes = ["Soft": 5, "Medium":7, "Hard":12]
+    
     @IBAction func hardnessSelected(_ sender: UIButton) {
        
-        let hardness = sender.currentTitle
+        let hardness = sender.currentTitle!
+        print (eggTimes[hardness]!)
+      
     }
     
 }
+   
